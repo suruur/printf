4cf2590f34978442a272e3603eebaf9441332f27
suruur <surrarweesh@gmail.com>
2023-10-20 23:45:09 +0000

    adding printf

---
+++ printf.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "string.h"
+#include "main.h"
+
+/**
+ * _printf - print
+ * @format: first arg
+ * @:
+ * Return: int
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, count, d;
+
+	count = 0;
+	va_start(args, format);
+
+	va_end(args);
+
+	return (count);
+}
